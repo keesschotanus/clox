2bafaf97166b37c12b6c352079fda499a600fab9
keesschotanus <kees.schotanus@planet.nl>
2026-07-12 09:08:19 +0200

    17.3 Emitting bytecode

--- src/compiler.c
+++ src/compiler.c
@@ -15,6 +15,12 @@ typedef struct {
 
 Parser parser;
 
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {
+	return compilingChunk;
+}
+
 static void errorAt(Token* token, const char* message) {
 	if (parser.panicMode) return;
 	parser.panicMode = true;
@@ -60,10 +66,31 @@ static void consume(TokenType type, const char* message) {
 	errorAtCurrent(message);
 }
 
+static void emitByte(uint8_t byte) {
+	writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+	emitByte(byte1);
+	emitByte(byte2);
+}
+
+static void emitReturn() {
+	emitByte(OP_RETURN);
+}
+
+static void endCompiler() {
+	emitReturn();
+}
+
+static void expression() {
+	// What goes here?
+}
 
 bool compile(const char *source, Chunk* chunk)
 {
 	initScanner(source);
+	compilingChunk = chunk;
 
 	parser.hadError = false;
 	parser.panicMode = false;
@@ -72,5 +99,6 @@ bool compile(const char *source, Chunk* chunk)
 	expression();
 	consume(TOKEN_EOF, "Expect end of expression");
 
+	endCompiler();
 	return !parser.hadError;
 }
